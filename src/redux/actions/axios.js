7c54a8718a2e043b3fb6122a63a47ebac0141d84
krushna <ksd@narola.email>
2020-12-12 09:11:26 +0530

    font modification_4

--- src/redux/actions/axios.js
+++ src/redux/actions/axios.js
@@ -4,8 +4,8 @@ const token = localStorage.getItem("access_token");
 
 if (window.location.host === "localhost:3000") {
   // axios.defaults.baseURL = "https://devapi.onon.life";
-  axios.defaults.baseURL = "https://192.168.100.39:8000";
-  // axios.defaults.baseURL = "https://localhost:8000";
+  axios.defaults.baseURL = "http://192.168.100.39:8000";
+  // axios.defaults.baseURL = "http://localhost:8000";
 } else {
   axios.defaults.baseURL = "https://devapi.onon.life";
 }
