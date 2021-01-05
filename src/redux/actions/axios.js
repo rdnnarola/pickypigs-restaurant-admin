0fb4e17cf3b3ffe0fa8bf67788e7c3b3870d7727
krushna <ksd@narola.email>
2021-01-05 09:18:45 +0530

    change@URL

--- src/redux/actions/axios.js
+++ src/redux/actions/axios.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const token = localStorage.getItem("access_token");
 
 if (window.location.host === "localhost:3000") {
-  axios.defaults.baseURL = "https://pickypigsapi.herokuapp.com/";
+  axios.defaults.baseURL = "https://pickypigsapi.herokuapp.com";
   // axios.defaults.baseURL = "http://192.168.100.39:8000";
   // axios.defaults.baseURL = "http://localhost:8000";
 } else {
-  axios.defaults.baseURL = "http://192.168.100.39:8000";
+  axios.defaults.baseURL = "https://pickypigsapi.herokuapp.com";
 }
 
 if (token) axios.defaults.headers.common = { "x-access-token": token };
