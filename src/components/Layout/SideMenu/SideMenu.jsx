37a25696a4ff2203a7ce88a2a90ee538c75a1f68
krushna <ksd@narola.email>
2021-01-15 10:13:49 +0530

    side_menu_change

--- src/components/Layout/SideMenu/SideMenu.jsx
+++ src/components/Layout/SideMenu/SideMenu.jsx
@@ -27,13 +27,13 @@ const SideMenu = () => {
                         </div>
                         <div className={`submenu_area  ${display==="block"?"d-block flex-column":"d-none"}`}>
                             <NavLink to="/breakfast_menu" className="submenu_name">Breakfast</NavLink>
-                            <NavLink to="/manage_menu" className="submenu_name">Manage Menus</NavLink>
-                            <NavLink to="/manage_submenu" className="submenu_name">Manage Sub Menus</NavLink>
-                            <NavLink to="/manage_categories" className="submenu_name">Manage Categories</NavLink>
-                            <NavLink to="/manage_subcategories" className="submenu_name">Manage Sub Categories</NavLink>
+                            {/* <NavLink to="/manage_menu" className="submenu_name">Manage Menus</NavLink> */}
+                            {/* <NavLink to="/manage_submenu" className="submenu_name">Manage Sub Menus</NavLink> */}
+                            {/* <NavLink to="/manage_categories" className="submenu_name">Manage Categories</NavLink> */}
+                            {/* <NavLink to="/manage_subcategories" className="submenu_name">Manage Sub Categories</NavLink> */}
                             <NavLink to="/all_dishes" className="submenu_name">All Dishes</NavLink>
                             <NavLink to="/manage_dishes" className="submenu_name">Manage Easy Add Dish</NavLink>
-                            <NavLink to="/restaurant_detail" className="submenu_name">Restaurant Setting</NavLink>
+                            {/* <NavLink to="/restaurant_detail" className="submenu_name">Restaurant Setting</NavLink> */}
                             <NavLink to="/" className="submenu_name">Dietary & Lifestyle</NavLink>
                         </div>
 
@@ -41,27 +41,34 @@ const SideMenu = () => {
                             <div className="menu-icon">
                                 <img src={menu_icon} className="img-fluid" alt="icon"  />
                             </div>
-                            <button className="btn btn-none text-primary menu-name">
-                                Menus
-                        </button>
+                            <NavLink to="/manage_menu" className="btn btn-none text-primary menu-name">Menus</NavLink>
+                        </div>
+                        <div className="menu-list d-flex align-items-center">
+                            <div className="menu-icon">
+                                <img src={menu_icon} className="img-fluid" alt="icon"  />
+                            </div>
+                            <NavLink to="/manage_submenu" className="btn btn-none text-primary menu-name">Sub Menus</NavLink>
                         </div>
                         <div className="menu-list d-flex align-items-center">
                             <div className="menu-icon">
                                 <img src={category_icon} className="img-fluid" alt="icon"  />
                             </div>
-                            <button className="btn btn-none text-primary menu-name">
-                                Categories
-                        </button>
+                            <NavLink to="/manage_categories" className="btn btn-none text-primary menu-name">Categories</NavLink>
                         </div>
+                        <div className="menu-list d-flex align-items-center">
+                            <div className="menu-icon">
+                                <img src={category_icon} className="img-fluid" alt="icon"  />
+                            </div>
+                            <NavLink to="/manage_subcategories" className="btn btn-none text-primary menu-name">Sub Categories</NavLink>
+                        </div>
+                       
                     </div>
                     <div className="aside-footer">
                         <div className="menu-list d-flex align-items-center">
                             <div className="menu-icon">
                                 <img src={setting_icon} className="img-fluid" alt="icon"/>
                             </div>
-                            <button className="btn btn-none text-primary menu-name">
-                                Settings
-                            </button>
+                            <NavLink to="/restaurant_detail" className="btn btn-none text-primary menu-name">Settings</NavLink>
                         </div>
                         <p className="copyright-txt">Copyrights © nareshbingi.com</p>
                     </div>
